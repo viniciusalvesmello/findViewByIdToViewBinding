f1730d8a61621364ab4e0379fbe01cffd544f657
Vinícius Alves Mello <vinicius@fretebras.com.br>
2020-04-02 00:03:37 -0300

    Find View By Id

--- app/src/main/java/io/github/viniciusalvesmello/findviewbyidtoviewbinding/view/FindViewByIdFragment.kt
+++ app/src/main/java/io/github/viniciusalvesmello/findviewbyidtoviewbinding/view/FindViewByIdFragment.kt
@@ -1,10 +1,12 @@
 package io.github.viniciusalvesmello.findviewbyidtoviewbinding.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import io.github.viniciusalvesmello.findviewbyidtoviewbinding.R
@@ -14,25 +16,36 @@ class FindViewByIdFragment : Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
 
+    private lateinit var tvFindViewByIdCounter: AppCompatTextView
+    private lateinit var bFindViewByIdIncCounter: AppCompatButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_find_view_by_id, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_find_view_by_id, container, false)
+
+        tvFindViewByIdCounter = view.findViewById(R.id.tvFindViewByIdCounter)
+        bFindViewByIdIncCounter = view.findViewById(R.id.bFindViewByIdIncCounter)
+
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initListener()
         initObserver()
     }
 
     private fun initListener() {
-        //TODO ON CLICK LISTENER
+        bFindViewByIdIncCounter.setOnClickListener {
+            viewModel.incCounter()
+        }
     }
 
     private fun initObserver() {
         viewModel.counter.observe(viewLifecycleOwner, Observer {
-            //TODO UPDATE UI
+            tvFindViewByIdCounter.text = (it ?: 0).toString()
         })
     }
 
