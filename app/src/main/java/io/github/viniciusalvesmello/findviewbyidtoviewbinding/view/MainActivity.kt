ce55d61681af3de8f3bf96d35ba0bf1fed553d84
Vinícius Alves Mello <vinicius.ufg@gmail.com>
2020-04-05 13:04:36 -0300

    Initial State

---
+++ app/src/main/java/io/github/viniciusalvesmello/findviewbyidtoviewbinding/view/MainActivity.kt
@@ -0,0 +1,39 @@
+package io.github.viniciusalvesmello.findviewbyidtoviewbinding.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import io.github.viniciusalvesmello.findviewbyidtoviewbinding.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        bFindViewById.setOnClickListener {
+            initFragment(FindViewByIdFragment.newInstance())
+        }
+        bDataBinding.setOnClickListener {
+            initFragment(DataBindingFragment.newInstance())
+        }
+        bKotlinSynthetic.setOnClickListener {
+            initFragment(KotlinSyntheticFragment.newInstance())
+        }
+        bViewBinding.setOnClickListener {
+            initFragment(ViewBindingFragment.newInstance())
+        }
+    }
+
+    private fun initFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+    }
+}
